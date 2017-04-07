82d627e9551ec19b80d6bbfc2c3b5a45497b851c
Valentin Levesy <valentin.levesy@ensimag.grenoble-inp.fr>
2017-04-07 19:27:55 +0200

    Ajout proposition

--- src/main/java/controleur/GameControleur.java
+++ src/main/java/controleur/GameControleur.java
@@ -101,7 +101,7 @@ public class GameControleur extends HttpServlet {
         
         
         
-        List<Player> players = playerDAO.getListPlayers(gameID);
+        List<Player> players = playerDAO.getListPlayersProposable(gameID);
         System.out.println(players);
         request.setAttribute("playerList", players);
 
@@ -125,9 +125,9 @@ public class GameControleur extends HttpServlet {
             throws ServletException, IOException {
 
         int userId = Integer.parseInt(request.getParameter("toProposeId"));
-        int gameId = Integer.parseInt(request.getParameter("gameId"));
+        int gameId = SessionManager.getGameSession(request);
         
-        playerDAO.proposer(userId, gameId);
+        playerDAO.proposer(userId);
         actionAfficher(request, response, gameDAO, playerDAO);
 
     }
